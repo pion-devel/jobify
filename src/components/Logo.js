1616915d95d4f4c3402eee43c907fea49737e6e5
fouadliady <mycontactpro@yahoo.com>
2023-10-10 12:53:36 +0200

    main commit

---
+++ src/components/Logo.js
@@ -0,0 +1,10 @@
+import logo from "../assets/images/logo.svg";
+
+const Logo = () => {
+  return (
+    <div style={{ fontSize: "3rem", fontWeight: "bolder", color: "#2563eb" }}>
+      JOBIFY
+    </div>
+  );
+};
+export default Logo;
---
+++ src/pages/Landing.js
@@ -0,0 +1,34 @@
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components";
+import { Link } from "react-router-dom";
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            The Job Tracking App is a user-friendly and efficient digital tool
+            designed to help individuals and teams manage their tasks,
+            assignments, and projects effectively. With intuitive features and a
+            streamlined interface, this app empowers users to stay organized and
+            focused on their work.
+          </p>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
